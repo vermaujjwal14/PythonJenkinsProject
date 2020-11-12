ef1314711ff4852aaee6c0cac1909c20f5b38b73
vermaujjwal14 <33069761+vermaujjwal14@users.noreply.github.com>
2020-11-12 18:19:25 +0530

    Update JenkinsPythonProgram.py

--- JenkinsPythonProgram.py
+++ JenkinsPythonProgram.py
@@ -1,51 +1,10 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "The count of color: green is  3\n"
-     ]
-    }
-   ],
-   "source": [
-    "list1 = ['red', 'green', 'blue', 'orange', 'green', 'gray', 'green']\n",
-    "color_count = list1.count('green')\n",
-    "print('The count of color: green is ', color_count)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
